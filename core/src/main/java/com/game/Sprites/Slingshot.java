91df491e567aa61c6aa533b96459df883c7a0edb
G_varsha <varshaganesh2645@gmail.com>
2024-11-26 16:50:27 +0300

    updated sprites

--- core/src/main/java/com/game/Sprites/Slingshot.java
+++ core/src/main/java/com/game/Sprites/Slingshot.java
@@ -1,20 +1,25 @@
 package com.game.Sprites;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class Slingshot extends Sprite {
+public class Slingshot extends Actor {
     private Texture texture;
 
     public Slingshot(float x, float y) {
         texture = new Texture("Slingshot.png");
-        setTexture(texture);
+        setSize(texture.getWidth(), texture.getHeight());
         setPosition(x,y);
     }
 
     public void draw(SpriteBatch spritebatch){
         spritebatch.draw(texture, getX(), getY(), getWidth(), getHeight());
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
     }
 
     public void dispose() {
@@ -23,5 +28,3 @@ public class Slingshot extends Sprite {
 
 
 }
-
-
